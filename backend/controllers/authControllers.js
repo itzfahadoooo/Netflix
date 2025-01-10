986ae6685e1c31f4c66eefede0ad30183aedc61e
itzfahadoooo <foyindolapo@gmail.com>
2025-01-10 22:39:24 +0100

    Initial commit

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -28,13 +28,21 @@ export async function signup(req,res){
       return res.status(400).json({success:false,message:"User with this name already exists"});
     }
 
+    const PROFILE_PICS=["/avatar1.png","/avatar2.png","/avatar3.png"]
+
+    const image = PROFILE_PICS[Math.floor(Math.random()*PROFILE_PICS.length)];
+
     const newUser = new User({
-      email:email,
-      password:password,
-      username:username,
+      email,
+      password,
+      username,
+      image
     });
+
+    await newUser.save();
   } catch (error) {
-    
+    console.log("Error in signup controller",error.message);
+    res.status(500).json({success:false,message:"Internal server error"});
   }
 }
 
