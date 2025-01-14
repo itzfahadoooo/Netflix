efc79be676846cdca6964a94deba6cb80f4d7699
itzfahadoooo <foyindolapo@gmail.com>
2025-01-14 21:24:12 +0100

    Initial commit

--- backend/utils/generateTokens.js
+++ backend/utils/generateTokens.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
+    
   const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, {
     expiresIn: "15d",
   });
