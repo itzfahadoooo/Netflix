4132ed1cb1fc3fd769e9de4083234c3dc772006b
itzfahadoooo <foyindolapo@gmail.com>
2025-01-16 21:04:20 +0100

    Initial commit

--- backend/utils/generateTokens.js
+++ backend/utils/generateTokens.js
@@ -2,15 +2,9 @@ import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
-
   const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, {
     expiresIn: "15d",
   });
-
-
-
-  
-
 
   res.cookie("jwt-netflix", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
