2d9ea4ce126d2b172b2f846331c4dce2fc3f71cf
itzfahadoooo <foyindolapo@gmail.com>
2025-01-18 17:08:03 +0100

    Initial commit

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import { ENV_VARS } from '../config/envVars.js';
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies["jwt-netflix"]
+
+        if (!token) {
+            return res.status(401).json({success: false, message: "You need to be logged in to access this route" });
+        }
+
+        const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
+
+        if(!decoded){
+            return res.status(401).json({success: false, message: "Unauthorized - Invalid token" });
+        }
+
+        const user=await User.findById(decoded.id).select("-password");
+
+        if(!user){
+            return res.status(404).json({success: false, message: "User not found" });
+        }
+
+        req.user=user;
+
+        next();
+    } catch (error) {
+        
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,18 @@ import { connect } from "mongoose";
 import { connectDB } from "./config/db.js";
 import movieRoutes from "./routes/movie.routes.js";
 import tvRoutes from "./routes/tv.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+
 
 app.listen(PORT, () => {
   console.log("Server running on http://localhost:" + PORT);
