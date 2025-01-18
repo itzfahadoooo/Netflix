16d7fc1b0ab0cda87735990f5277dcc65d0cf6a5
itzfahadoooo <foyindolapo@gmail.com>
2025-01-18 06:27:41 +0100

    Initial commit

--- backend/routes/tv.routes.js
+++ backend/routes/tv.routes.js
@@ -2,4 +2,12 @@ import express from 'express';
 
 const router = express.Router();
 
+
+router.get("/trending", getTrendingTv);
+router.get("/:id/trailers", getTvTrailers);
+router.get("/:id/details", getTvDetails);
+router.get("/:id/similar", getSimilarTvs);
+router.get("/:category", getTvsByCategory);
+
+
 export default router;
