2f81338ed9d4cc719839e3f828e965856f226654
itzfahadoooo <foyindolapo@gmail.com>
2025-01-18 06:25:50 +0100

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { ENV_VARS } from "./config/envVars.js";
 import { connect } from "mongoose";
 import { connectDB } from "./config/db.js";
 import movieRoutes from "./routes/movie.routes.js";
+import tvRoutes from "./routes/tv.routes.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on http://localhost:" + PORT);
