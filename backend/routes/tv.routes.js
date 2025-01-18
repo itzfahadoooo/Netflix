03ce6ebb4a4e2bc5c6efdc49159997538c8bae03
itzfahadoooo <foyindolapo@gmail.com>
2025-01-18 06:31:06 +0100

    Initial commit

--- backend/routes/tv.routes.js
+++ backend/routes/tv.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getSimilarTvs, getTrendingTv, getTvDetails, getTvTrailers } from '../controllers/tv.controller';
+import { getSimilarTvs, getTrendingTv, getTvDetails, getTvsByCategory, getTvTrailers } from '../controllers/tv.controller';
 
 const router = express.Router();
 
