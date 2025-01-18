2426817a1dd33dc58add9861cd935f5d4ffff6d3
itzfahadoooo <foyindolapo@gmail.com>
2025-01-18 17:09:00 +0100

    Initial commit

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -26,6 +26,7 @@ export const protectRoute = async (req, res, next) => {
 
         next();
     } catch (error) {
-        
+        console.log("Error in protectRoute middleware: ", error.message);
+        res.status(500).json({success: false, message: "Internal server error" });
     }
 }
