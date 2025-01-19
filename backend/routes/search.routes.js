0824469669b216e2f99f74cbdbc9e850b11826b9
itzfahadoooo <foyindolapo@gmail.com>
2025-01-19 12:41:36 +0100

    Initial commit

--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { searchMovie, searchPerson, searchTv } from '../controllers/search.controller.js';
+import { get } from 'mongoose';
 
 const router = express.Router();
 
 router.get("/person/:query", searchPerson);
 router.get("/movie/:query", searchMovie);
 router.get("/tv/:query", searchTv);
+
+router.get("/history", getSearchHistory);
+
+router.delete("/history/:id", removeitemfromsearchHistory);
 
 export default router;
