e94deb884ed89be9651823a9ffe06e0d039d74fc
itzfahadoooo <foyindolapo@gmail.com>
2025-01-19 12:34:29 +0100

    Initial commit

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,3 +1,6 @@
+import { fetchfromTMDB } from "../../services/tmdb.service.js";
+import { User } from "../models/user.model.js";
+
 export async function searchPerson(req, res) {
     const { query } = req.params;
     try {
@@ -7,14 +10,47 @@ export async function searchPerson(req, res) {
             return res.status(404).json({message: "No results found"})
         }
 
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: {
+                searchHistory: {
+                    id: response.results[0].id,
+                    image: response.results[0].profile_path,
+                    title: response.results[0].name,
+                    searchType: "person",
+                    createdAt: new Date(),
+                }}}),
+
         res.status(200).json({success:true, content: response.results})
     } catch (error) {
         console.log("Error in searchPersoon controller",error.message)
-        res.status(500).json({message: "Internal server error"})
+        res.status(500).json({success: false, message: "Internal server error"})
     }
 }
 
 
-export async function searchMovie(req, res) {}
+export async function searchMovie(req, res) {
+    const { query } = req.params;
+    try {
+        const response = await fetchfromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`)
+
+        if (response.results.length === 0) {
+            return res.status(404).json({message: "No results found"})
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: {
+                searchHistory: {
+                    id: response.results[0].id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].title,
+                    searchType: "movie",
+                    createdAt: new Date(),
+                }}}),    
+        res.status(200).json({success:true, content: response.results})    
+    } catch (error) {
+        console.log("Error in searchMovie controller",error.message)
+        res.status(500).json({success: false, message: "Internal server error"})
+    }
+}
 
 export async function searchTv(req, res) {}
