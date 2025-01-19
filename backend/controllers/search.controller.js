3e27fe93019953a63f0f582bac15455c3113ca47
itzfahadoooo <foyindolapo@gmail.com>
2025-01-19 12:37:36 +0100

    Initial commit

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -53,4 +53,28 @@ export async function searchMovie(req, res) {
     }
 }
 
-export async function searchTv(req, res) {}
+export async function searchTv(req, res) {
+    const { query } = req.params;
+
+    try {
+        const response = await fetchfromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`)
+
+        if (response.results.length === 0) {
+            return res.status(404).json({message: "No results found"})
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: {
+                searchHistory: {
+                    id: response.results[0].id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].name,
+                    searchType: "tv",
+                    createdAt: new Date(),
+                }}}),
+        res.status(200).json({success:true, content: response.results})
+    } catch (error) {
+        console.log("Error in searchTv controller",error.message)
+        res.status(500).json({success: false, message: "Internal server error"})
+    }
+}
