733b977773c6aa02790822b5124e928c238cdd89
itzfahadoooo <foyindolapo@gmail.com>
2025-01-19 06:18:53 +0100

    Initial commit

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,4 +1,18 @@
-export async function searchPerson(req, res) {}
+export async function searchPerson(req, res) {
+    const { query } = req.params;
+    try {
+        const response = await fetchfromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`)
+
+        if(response.results.length === 0) {
+            return res.status(404).json({message: "No results found"})
+        }
+
+        res.status(200).json({success:true, content: response.results})
+    } catch (error) {
+        console.log("Error in searchPersoon controller",error.message)
+        res.status(500).json({message: "Internal server error"})
+    }
+}
 
 
 export async function searchMovie(req, res) {}
