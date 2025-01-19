648f15b3d999b08bff788b954542272ed6772421
itzfahadoooo <foyindolapo@gmail.com>
2025-01-19 06:09:05 +0100

    Initial commit

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -1,5 +1,6 @@
 export async function searchPerson(req, res) {}
 
+
 export async function searchMovie(req, res) {}
 
-export async function searchTV(req, res) {}
+export async function searchTv(req, res) {}
--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { searchMovie, searchPerson, searchTv } from '../controllers/search.controller';
 
 const router = express.Router();
 
 router.get("/person/:query", searchPerson);
 router.get("/movie/:query", searchMovie);
-router.get("/tv/:query", searchTV);
+router.get("/tv/:query", searchTv);
 
 export default router;
