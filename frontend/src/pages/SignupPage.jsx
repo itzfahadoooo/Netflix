aca30c88f61142cd04e7a014ed13d90157c8320f
itzfahadoooo <foyindolapo@gmail.com>
2025-01-21 06:23:03 +0100

    Initial commit

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
-  )
-}
+    <div className="h-screen w-full hero-bg">
+      <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+        <Link to={"/"}>
+          <img src="/netflix-logo.png" alt="logo" className="w-52" />
+        </Link>
+      </header>
 
-export default SignupPage
+      <div className="flex items-center justify-center mt-20 mx-3">
+        <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
+          <h1 className="ext-center text-white text-2xl font-bold mb-4">Sign Up</h1>
+          <form className="space-y-4">
+              <div>
+                <label htmlFor="email" className="text-sm font-medium text-gray-300 block">
+                  Email
+                </label>
+                <input
+								type='email'
+								className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
+								placeholder='you@example.com'
+								id='email'
+								// value={email}
+								// onChange={(e) => setEmail(e.target.value)}
+							/>
+              </div>
+            </form> 
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
