81d9b399e2251e84abb35ada60f5baf865c6bd9c
itzfahadoooo <foyindolapo@gmail.com>
2025-01-22 19:37:17 +0100

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -13,7 +14,17 @@ function App() {
 
   useEffect(() => {
     authCheck();
-  }, []);
+  }, [authCheck]);
+
+  if (isCheckingAuth) {
+    return(
+      <div className="h-screen">
+        <div className="flex justify-center items-center bg-black h-full">
+          <Loader className="animate-spin text-red-600 size-10"/>
+        </div>
+      </div>
+    )
+  }
   return (
     <>
       <Routes>
