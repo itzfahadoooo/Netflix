b9df135be7e023fd47e0fd30bf43e87b4f2d2e4a
itzfahadoooo <foyindolapo@gmail.com>
2025-01-23 23:02:07 +0100

    Initial commit

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -6,7 +6,19 @@ const HomeScreen = () => {
       <Navbar />
 
       <img src="/extraction.jpg" alt="hero-img" className="absolute top-0 left-0 w-full h-full object-cover -z-50"/>
-      <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer" />
+
+      <div className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50" aria-hidden="true" />
+
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32" />
+
+      <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute top-0 left-0 w-full h-full -z-10" />
+
+      <div className="max-w-2xl">
+          <h1 className="mt-4 text-6xl font-extrabold text-balance">
+            Extraction
+          </h1>
+          <p className="mt-2 text-lg">2014 | 18+</p>
+      </div>
     </div>
   );
 };
