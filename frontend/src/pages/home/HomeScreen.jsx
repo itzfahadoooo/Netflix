c21c8c1f417f6510c31d6c3a959ebe755ede6d1a
itzfahadoooo <foyindolapo@gmail.com>
2025-01-23 06:34:21 +0100

    Initial commit

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,13 +1,14 @@
-import { useAuthStore } from "../../store/authUser"
+import Navbar from "../../components/Navbar";
 
 const HomeScreen = () => {
-  const {logout}=useAuthStore()
   return (
-    <div>HomeScreen
+    <div className="h-screen text-white relative">
+      <Navbar />
 
-      <button onClick={logout}>logout</button>
+      <img src="/extraction.jpg" alt="hero-img" className="absolute top-0 left-0 w-full h-full object-cover -z-50"/>
+      <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer" />
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
