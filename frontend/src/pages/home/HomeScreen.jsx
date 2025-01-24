cddebf6d10225cfcb2416a831bda37ce715f4e08
itzfahadoooo <foyindolapo@gmail.com>
2025-01-24 18:09:13 +0100

    Initial commit

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -34,7 +34,10 @@ const HomeScreen = () => {
         </div>
 
         <div className="flex mt-8">
-          <Link to="/watch/123">
+          <Link
+            to="/watch/123"
+            className="bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex items-center"
+          >
             <Play className="size-6 inline-block mr-2 fill-black" />
             Play
           </Link>
