144fb1097c38678c2ff1c2a083a0d3386585580c
itzfahadoooo <foyindolapo@gmail.com>
2025-01-25 07:40:23 +0100

    Initial commit

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,7 +1,9 @@
+import { useEffect } from "react";
 import { useContentStore } from "../store/content";
 
 const MovieSlider = ({ category }) => {
   const { contentType } = useContentStore();
+  const 
 
   const formattedCategoryName =
     category.replaceAll("_", " ")[0].toUpperCase() +
@@ -9,7 +11,14 @@ const MovieSlider = ({ category }) => {
 
   const formattedContentType = contentType === "movie" ? "Movies" : "TV Shows";
 
-  
+  useEffect(() => {
+    const getContent = async () => {
+        const res = await axios.get(`/api/v1/${contentType}/${category}`);
+        setContent(res.data.content);
+    };
+
+    getContent();
+}, [contentType, category]);
   return (
     <div className="text-white bg-black relative px-5 md:px-20">
       <h2>
