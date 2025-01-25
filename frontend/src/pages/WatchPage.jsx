1c8d2dd12af8662dad2afcd2d766baebff43671c
itzfahadoooo <foyindolapo@gmail.com>
2025-01-25 17:59:09 +0100

    Initial commit

---
+++ frontend/src/pages/WatchPage.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useContentStore } from "../store/content";
+import axios from "axios";
+import { set } from "mongoose";
+
+const WatchPage = () => {
+  const { id } = useParams();
+  const [trailers, setTrailers] = useState([]);
+  const [currentTrailerIdx, setCurrentTrailerIdx] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [content, setContent] = useState({});
+  const { contentType } = useContentStore();
+
+  useEffect(() => {
+    const getTrailers = async () => {
+      try {
+        const res = await axios.get(`/api/v1/${contentType}/${id}/trailers`);
+        setTrailers(res.data.trailers);
+      } catch (error) {
+        if (error.message.includes("404")) {
+          console.log("No trailers found");
+          setTrailers([]);
+        }
+      }
+    };
+  }, [contentType, id]);
+  return <div>WatchPage</div>;
+};
+
+export default WatchPage;
