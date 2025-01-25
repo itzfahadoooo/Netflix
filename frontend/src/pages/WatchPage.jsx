89cf2dcf9a1db62855d8dc5f4f2928bc056fab31
itzfahadoooo <foyindolapo@gmail.com>
2025-01-25 22:17:58 +0100

    Initial commit

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import Navbar from "../components/Navbar";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import ReactPlayer from "react-player";
+import { ORIGINAL_IMG_BASE_URL } from "../utils/constants";
+import { formatReleaseDate } from "../utils/dateFunction";
 
 const WatchPage = () => {
   const { id } = useParams();
