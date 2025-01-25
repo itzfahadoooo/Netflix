064ff8d22257d6806415bcf22e22199226131e57
itzfahadoooo <foyindolapo@gmail.com>
2025-01-25 17:44:19 +0100

    Initial commit

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -2,13 +2,19 @@ import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import { Info, Play } from "lucide-react";
 import useGetTrendingContent from "../../hooks/useGetTrendingContent";
-import { MOVIE_CATEGORIES, ORIGINAL_IMG_BASE_URL, TV_CATEGORIES } from "../../utils/constants";
+import {
+  MOVIE_CATEGORIES,
+  ORIGINAL_IMG_BASE_URL,
+  TV_CATEGORIES,
+} from "../../utils/constants";
 import { useContentStore } from "../../store/content";
 import MovieSlider from "../../components/MovieSlider";
+import { useState } from "react";
 
 const HomeScreen = () => {
   const { trendingContent } = useGetTrendingContent();
-  const {contentType}=useContentStore()
+  const { contentType } = useContentStore();
+  const [imgLoading, setImgLoading] = useState(true);
 
   if (!trendingContent)
     return (
@@ -23,10 +29,18 @@ const HomeScreen = () => {
       <div className="h-screen text-white relative">
         <Navbar />
 
+        {/* COOL OPTIMIZATION HACK FOR IMAGES */}
+        {imgLoading && (
+          <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center shimmer -z-10" />
+        )}
+
         <img
           src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
           alt="hero-img"
           className="absolute top-0 left-0 w-full h-full object-cover -z-50"
+          onLoad={() => {
+            setImgLoading(false);
+          }}
         />
 
         <div
@@ -74,11 +88,15 @@ const HomeScreen = () => {
         </div>
       </div>
 
-      <div className='flex flex-col gap-10 bg-black py-10'>
-				{contentType === "movie"
-					? MOVIE_CATEGORIES.map((category) => <MovieSlider key={category} category={category} />)
-					: TV_CATEGORIES.map((category) => <MovieSlider key={category} category={category} />)}
-			</div>
+      <div className="flex flex-col gap-10 bg-black py-10">
+        {contentType === "movie"
+          ? MOVIE_CATEGORIES.map((category) => (
+              <MovieSlider key={category} category={category} />
+            ))
+          : TV_CATEGORIES.map((category) => (
+              <MovieSlider key={category} category={category} />
+            ))}
+      </div>
     </>
   );
 };
