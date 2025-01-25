668789fce402e167e9e9522a24c2306cda5cbe02
itzfahadoooo <foyindolapo@gmail.com>
2025-01-25 12:27:18 +0100

    Initial commit

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useContentStore } from "../store/content";
+import { Link } from "react-router-dom";
+import { SMALL_IMG_BASE_URL } from "../utils/constants";
 
 const MovieSlider = ({ category }) => {
   const { contentType } = useContentStore();
-  const 
+  const { content, setContent } = useState([]);
 
   const formattedCategoryName =
     category.replaceAll("_", " ")[0].toUpperCase() +
@@ -13,17 +15,37 @@ const MovieSlider = ({ category }) => {
 
   useEffect(() => {
     const getContent = async () => {
-        const res = await axios.get(`/api/v1/${contentType}/${category}`);
-        setContent(res.data.content);
+      const res = await axios.get(`/api/v1/${contentType}/${category}`);
+      setContent(res.data.content);
     };
 
     getContent();
-}, [contentType, category]);
+  }, [contentType, category]);
   return (
     <div className="text-white bg-black relative px-5 md:px-20">
       <h2>
         {formattedCategoryName} {formattedContentType}
       </h2>
+
+      <div className="flex space-x-4">
+        {content.map((item) => (
+          <Link
+            to={`/watch/${item.id}`}
+            className="min-w-[250px] relative group"
+            key={item.id}
+          >
+            <div className="rounded-lg overflow-hidden">
+              <img
+                src={SMALL_IMG_BASE_URL + item.backdrop_path}
+                alt="Movie image"
+                className="transition-transform duration-300 ease-in-out group-hover:scale-125"
+              />
+            </div>
+
+            <p className="mt-2 text-center">{item.title || item.name}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
