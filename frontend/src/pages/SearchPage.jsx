bc2532ca0070ebe706240c1426b06d964cc918e4
itzfahadoooo <foyindolapo@gmail.com>
2025-01-26 10:53:58 +0100

    Initial commit

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
+import { useContentStore } from '../store/content';
 
 const SearchPage = () => {
+    const [activeTab, setActiveTab] = useState("movie");
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const [results, setResults] = useState([]);
+	const { setContentType } = useContentStore();
   return (
+    
     <div>SearchPage</div>
   )
 }
