ce31179cb8a055a91f1f9a7234544cf527838441
itzfahadoooo <foyindolapo@gmail.com>
2025-01-26 10:28:03 +0100

    Initial commit

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useContentStore } from "../store/content";
 import axios from "axios";
 import Navbar from "../components/Navbar";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import ReactPlayer from "react-player";
-import { ORIGINAL_IMG_BASE_URL } from "../utils/constants";
+import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from "../utils/constants";
 import { formatReleaseDate } from "../utils/dateFunction";
 
 const WatchPage = () => {
@@ -16,6 +16,8 @@ const WatchPage = () => {
   const [content, setContent] = useState({});
   const [similarContent, setSimilarContent] = useState([]);
   const { contentType } = useContentStore();
+
+  const sliderRef = useRef(null);
 
   useEffect(() => {
     const getTrailers = async () => {
@@ -88,6 +90,21 @@ const WatchPage = () => {
   };
   const handlePrev = () => {
     if (currentTrailerIdx > 0) setCurrentTrailerIdx(currentTrailerIdx - 1);
+  };
+
+  const scrollLeft = () => {
+    if (sliderRef.current)
+      sliderRef.current.scrollBy({
+        left: -sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+  };
+  const scrollRight = () => {
+    if (sliderRef.current)
+      sliderRef.current.scrollBy({
+        left: sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
   };
 
   return (
@@ -172,6 +189,50 @@ const WatchPage = () => {
             className="max-h-[600px] rounded-md"
           />
         </div>
+
+        {similarContent.length > 0 && (
+          <div className="mt-12 max-w-5xl mx-auto relative">
+            <h3 className="text-3xl font-bold mb-4">Similar Movies/Tv Show</h3>
+
+            <div
+              className="flex overflow-x-scroll scrollbar-hide gap-4 pb-4 group"
+              ref={sliderRef}
+            >
+              {similarContent.map((content) => {
+                if (content.poster_path === null) return null;
+                return (
+                  <Link
+                    key={content.id}
+                    to={`/watch/${content.id}`}
+                    className="w-52 flex-none"
+                  >
+                    <img
+                      src={SMALL_IMG_BASE_URL + content.poster_path}
+                      alt="Poster path"
+                      className="w-full h-auto rounded-md"
+                    />
+                    <h4 className="mt-2 text-lg font-semibold">
+                      {content.title || content.name}
+                    </h4>
+                  </Link>
+                );
+              })}
+
+              <ChevronRight
+                className="absolute top-1/2 -translate-y-1/2 right-2 w-8 h-8
+										opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer
+										 bg-red-600 text-white rounded-full"
+                onClick={scrollRight}
+              />
+              <ChevronLeft
+                className="absolute top-1/2 -translate-y-1/2 left-2 w-8 h-8 opacity-0 
+								group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-red-600 
+								text-white rounded-full"
+                onClick={scrollLeft}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
