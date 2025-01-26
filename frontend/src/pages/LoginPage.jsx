c5bdd5936cbe9a368bb104d9b888e4f7a961b54d
itzfahadoooo <foyindolapo@gmail.com>
2025-01-26 22:22:55 +0100

    Initial commit

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,7 +6,9 @@ const LoginPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	const { login } = useAuthStore();
+	
+	const { login, isLoggingIn } = useAuthStore();
+
 
 	const handleLogin = (e) => {
 		e.preventDefault();
@@ -58,9 +60,9 @@ const LoginPage = () => {
 							className='w-full py-2 bg-red-600 text-white font-semibold rounded-md
 							hover:bg-red-700
 						'
-							// disabled={isLoggingIn}
+							disabled={isLoggingIn}
 						> Login
-							{/* {isLoggingIn ? "Loading..." : "Login"} */}
+							{isLoggingIn ? "Loading..." : "Login"}
 						</button>
 					</form>
 					<div className='text-center text-gray-400'>
