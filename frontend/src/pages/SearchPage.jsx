40ecee670cc00ca5b2dfb84699ba1ca6c365c8fc
itzfahadoooo <foyindolapo@gmail.com>
2025-01-26 10:59:39 +0100

    Initial commit

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,16 +1,53 @@
-import { useState } from 'react'
-import { useContentStore } from '../store/content';
+import { useState } from "react";
+import { useContentStore } from "../store/content";
+import Navbar from "../components/Navbar";
 
 const SearchPage = () => {
-    const [activeTab, setActiveTab] = useState("movie");
-	const [searchTerm, setSearchTerm] = useState("");
+  const [activeTab, setActiveTab] = useState("movie");
+  const [searchTerm, setSearchTerm] = useState("");
 
-	const [results, setResults] = useState([]);
-	const { setContentType } = useContentStore();
+  const [results, setResults] = useState([]);
+  const { setContentType } = useContentStore();
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+    tab === "movie" ? setContentType("movie") : setContentType("tv");
+    setResults([]);
+  };
+
   return (
-    
-    <div>SearchPage</div>
-  )
-}
+    <div className="bg-black min-h-screen text-white">
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex justify-center gap-3 mb-4">
+          <button
+            className={`py-2 px-4 rounded ${
+              activeTab === "movie" ? "bg-red-600" : "bg-gray-800"
+            } hover:bg-red-700`}
+            onClick={() => handleTabClick("movie")}
+          >
+            Movies
+          </button>
+          <button
+            className={`py-2 px-4 rounded ${
+              activeTab === "tv" ? "bg-red-600" : "bg-gray-800"
+            } hover:bg-red-700`}
+            onClick={() => handleTabClick("tv")}
+          >
+            TV Shows
+          </button>
+          <button
+            className={`py-2 px-4 rounded ${
+              activeTab === "person" ? "bg-red-600" : "bg-gray-800"
+            } hover:bg-red-700`}
+            onClick={() => handleTabClick("person")}
+          >
+            Person
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
