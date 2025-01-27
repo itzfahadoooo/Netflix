a937c4f9df87ab50f2654db4f46c1d86c6990f90
itzfahadoooo <foyindolapo@gmail.com>
2025-01-27 10:33:01 +0100

    Initial commit

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -7,5 +7,6 @@ export const ENV_VARS = {
   PORT: process.env.PORT || 5000,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
-  TMDB_API_KEY:process.env.TMDB_API_KEY,
+  TMDB_API_KEY: process.env.TMDB_API_KEY,
+  FRONTEND_URL: process.env.FRONTEND_URL,
 };
