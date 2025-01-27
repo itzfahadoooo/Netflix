cf7fcf5f74a389199d4cee2fbf2c8a5a0f1ddeb9
itzfahadoooo <foyindolapo@gmail.com>
2025-01-27 11:26:39 +0100

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -23,19 +23,6 @@ app.use(
   })
 ); // Enable CORS for your frontend
 
-app.use(cors({ origin: '*' }));; // Preflight handling
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://netflix-clone-nine-swart-76.vercel.app"
-  ); // Frontend URL
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allowed methods
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allowed headers
-  res.header("Access-Control-Allow-Credentials", "true"); // Allow cookies
-  next();
-});
-
 app.use(express.json());
 
 // API Creation Endpoint
